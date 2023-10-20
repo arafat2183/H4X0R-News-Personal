57a925dead62c39c17ce757ace395cdfc17e7c96
arafat2183 <akash076987@gmail.com>
2023-10-20 17:15:56 +0900

    Initial commit

---
+++ H4X0R News Personal/H4X0R_News_PersonalApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_News_PersonalApp.swift
+//  H4X0R News Personal
+//
+//  Created by Akash Arafat on 2023/10/20.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_News_PersonalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
